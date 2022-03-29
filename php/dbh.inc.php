4ca5a7465c231f268fb6f577fa67e8502acb5ce7
halsowadi <halsowadi1@gmail.com>
2022-03-29 18:08:41 -0400

    more php connection stuff

---
+++ php/dbh.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword= "Chicken1!";
+$dbName = "instacart";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+
+
+
+
+
+
+?>
