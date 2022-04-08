694e9373f96927a95b4d0f87f918ca3ebbf3e8a8
halsowadi <halsowadi1@gmail.com>
2022-04-08 15:36:18 -0400

    Got delete function to work for adminpage

--- FrontEnd/admin_page.php
+++ FrontEnd/admin_page.php
@@ -1,19 +1,20 @@
 <?php
 
 @include 'config.php';
-session_start();
 if(isset($_POST['add_product'])){
 
    $product_name = $_POST['product_name'];
    $product_price = $_POST['product_price'];
-   $product_stock = 100;
    $product_ID = $_POST['product_ID'];
+   $product_stock = 100;
  $product_matierals= "stuff";
 
+
+   //method to insert
    if(empty($product_name) || empty($product_price) ){
       $message[] = 'please fill out all';
    }else{
-      $insert = "INSERT INTO instacart.PRODUCT VALUES('$product_ID','$product_name','$product_matierals', 100 ,'$product_price')";
+      $insert = "INSERT INTO instacart.PRODUCT VALUES('$product_ID', '$product_name','$product_matierals', 100 ,'$product_price')";
       $upload = mysqli_query($conn,$insert);
       
       if($upload){
@@ -23,7 +24,17 @@ if(isset($_POST['add_product'])){
      }
    
    }
+};
 
+   //method to delete
+   if(isset($_GET['delete'])){
+      $id = $_GET['delete'];
+      $delete = "DELETE FROM PRODUCT WHERE Product_ID = $id";
+      
+      mysqli_query($conn, $delete);
+      
+      header('location:admin_page.php');
+   
 };
 
 
@@ -61,20 +72,45 @@ if(isset($message)){
 <div class="container">
 
    <div class="admin-product-form-container">
-
+      
       <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
          <h3>add a new product</h3>
          <input type="text" placeholder="enter product name" name="product_name" class="box">
          <input type="number" placeholder="enter product price" name="product_price" class="box">
          <input type="number" placeholder="enter product ID" name="product_ID" class="box">
          <input type="submit" class="btn" name="add_product" value="add product">
-      </form>
+         </form>
 
-   </div>
+</div>
 
-  
-      </table>
-   </div>
+<?php
+
+$select = mysqli_query($conn, "SELECT * FROM product");
+
+?>
+
+<div class="product-display">
+   <table class="product-display-table">
+      <thead>
+      <tr>
+         <th>product image</th>
+         <th>product name</th>
+         <th>product price</th>
+         <th>action</th>
+      </tr>
+      </thead>
+     
+      <?php while($row = mysqli_fetch_assoc($select)){ ?>
+      <tr>
+         <td><?php echo $row['product_name']; ?></td>
+         <td>$<?php echo $row['Price']; ?>/-</td>
+         <td>
+         <a href="admin_page.php?delete=<?php echo $row['Product_ID']; ?>" class="btn"  method="get" name = "delete"> <i class="fas fa-trash"></i> delete </a>
+         </td>
+      </tr>
+   <?php } ?>
+   </table>
+</div>
 
 </div>
 
